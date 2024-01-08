d32686e0f044a0aadc01b980547708a4ec4a0296
Lukas Karberg <deaderlp@googlemail.com>
2024-01-08 13:26:56 +0100

    flashcardsrounter mock in

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,8 +1,8 @@
 import * as config from "./config";
 import { app } from "./server";
 
-app.listen(config.getPort, () => {
+app.listen(config.getPort(), () => {
 	console.log(
-		`App Learn API is running at http://localhost:${config.getPort()}`
+		`AppLearn API is running at http://localhost:${config.getPort()}`
 	);
 });
--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { flashcardRouter } from "./routers/flashcardRouter";
 
 export const app = express();
 
 app.get("/", (req, res) => {
 	res.json({
-		appName: "API for App Learn version 0.1",
+		appName: "API for AppLearn version 0.1",
 	});
 });
+
+app.use("/api/flashcards", flashcardRouter);
