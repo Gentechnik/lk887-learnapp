25a089c296473a007d0ba71340ff09d31829368f
Lukas Karberg <deaderlp@googlemail.com>
2024-01-09 10:59:27 +0100

    Add CORS middleware to server and manage flashcards route

---
+++ src/pages/PageManageFlashcards.tsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
+export const PageManageFlashcards = () => {
+	const { flashcards } = useContext(AppContext);
+	return (
+		<>
+			<p>This is the info page with {flashcards.length} flashcards.</p>;
+			<table>
+				<thead>
+					<tr>
+						<th>SUUID</th>
+						<th>Category</th>
+						<th>Front</th>
+						<th>Back</th>
+					</tr>
+				</thead>
+				<tbody>
+					{flashcards.map((flashcard) => {
+						return (
+							<tr>
+								<td>{flashcard.suuid}</td>
+								<td>{flashcard.category}</td>
+								<td>{flashcard.front}</td>
+								<td>{flashcard.back}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</>
+	);
+};
