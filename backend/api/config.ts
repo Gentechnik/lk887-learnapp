c70506d255f21879ef6770b4391f797b9779dfe3
Lukas Karberg <deaderlp@googlemail.com>
2024-01-15 10:32:51 +0100

    Update API status and add maintenance mode middleware

--- backend/api/config.ts
+++ backend/api/config.ts
@@ -6,7 +6,7 @@ export const getPort = () => {
 
 export const apiStatus = (): TApiStatus => {
 	return {
-		status: "live",
+		status: "maintenanceMode",
 		whenLiveAgainDateTime: "",
 	};
 };
--- backend/api/middleware/maintenanceMode.ts
+++ backend/api/middleware/maintenanceMode.ts
@@ -8,9 +8,22 @@ export const maintenanceMode = (
 	next: express.NextFunction
 ) => {
 	// console.log("in maintenance mode middleware");
-	const apiStatus = config.apiStatus();
-	if (apiStatus.status === "maintenanceMode") {
-		res.status(503);
+	const { status, whenLiveAgainDateTime } = config.apiStatus();
+	if (status === "maintenanceMode") {
+		// res.status(503).send("site down");
+		const whenLiveAgainDayjs = dayjs(whenLiveAgainDateTime);
+		const secondsToGo = whenLiveAgainDayjs.diff(dayjs(), "seconds") + 60;
+		const minutes = Math.floor((secondsToGo % 3600) / 60);
+		const hours = Math.floor(secondsToGo / 3600);
+		const duration =
+			hours > 0
+				? `${hours} hours, ${minutes} minutes`
+				: `${minutes} minutes`;
+		res.header("Retry-After", String(secondsToGo));
+		res.status(503).send(
+			`This API is currently in maintenance mode. Expected up time is ${whenLiveAgainDateTime}. Retry in ${duration}.`
+		);
+	} else {
+		next();
 	}
-	next();
 };
